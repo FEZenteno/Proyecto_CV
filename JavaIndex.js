19a014ae6bd483860b3b7c05c30e9933de6101ad
Ezequiel Zenteno <eckiel.18@gmail.com>
2023-01-18 14:53:38 -0300

    Agregando responsive

--- JavaIndex.js
+++ JavaIndex.js
@@ -1,3 +1,11 @@
 function ocultar(){
     document.getElementById('').style.display = 'none';
     }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
